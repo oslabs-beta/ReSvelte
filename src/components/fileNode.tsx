0f154319e62b8e802374f79566dd4b305baff33c
sateeven <sateeven@gmail.com>
2022-04-18 19:12:09 -0700

    component tree algorithm almost finished
    Co-authored-by: Candie Hill <can330330@yahoo.com>
    Co-authored-by: Martin Ng <kamartinng@gmail.com>
    Co-authored-by: Hoon Park <hoon.park97@gmail.com>
    Co-authored-by: Jestyn Apuya <jestynapuya@gmail.com>

--- src/components/fileNode.tsx
+++ src/components/fileNode.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import '../styles.scss';
+import "../styles.scss";
 
-import { IoIosArrowDown, IoIosArrowUp  } from 'react-icons/io';
-import { AiFillFolder, AiFillFolderOpen } from 'react-icons/Ai';
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { AiFillFolder, AiFillFolderOpen } from "react-icons/ai";
 
-import getAliases from '../parser/getAliases';
+import getAliases from "../parser/getAliases";
 
 // filenode component takes in children and filename
 const FileNode = (props) => {
@@ -14,188 +14,200 @@ const FileNode = (props) => {
   const [showChildren, setShow] = useState(false);
   const [componentChildren, setChildren] = useState();
 
-  let aliases = {};
- // aliases = { importname: aliasfileName}
- // file
-// creates the tree; file is props
-  function createTree(){ // has access to the global props
+  let aliases = props.aliases;
+
+  // creates the tree; file is props
+  function createTree() {
+    // has access to the global props
     // search for a script tag and parse
-   
+    
+    console.log(`CREATING TREE FOR ${props.fileName}`)
+    console.log(aliases)
+
     for (let i = 0; i < props.children.length; i++) {
 
       // loop to generate aliases from out script tag
-      if (props.children[i].tagName === 'script') {
-        aliases = getAliases(props.children[i]);
-        console.log(`Aliases for ${props.children[i]}`, aliases);
+      if (props.children[i].type !== "svelteComponent" && props.children[i].type !== "svelteElement" ) {
         continue;
       }
       
-      if(props.children[i].type !== 'svelteComponent' && props.children[i].type !== 'svelteElement') {
-        continue;}
+      if (props.children[i].tagName === "script") {
+        aliases = getAliases(props.children[i]);
+        console.log(`Aliases for ${props.children[i].fileName}`, aliases);
+        continue;
+      }
 
-      console.log('this is props.svelteFiles 1', props.svelteFiles)
-      
-      if(props.children[i].type === 'svelteElement'){
-        childList.push(<FileNode children={props.children[i].children} fileName={props.children[i].tagName} fileType={'svelteElement'} svelteFiles={props.svelteFiles}/>);
-      } 
-      else{
-        console.log('this is props.svelteFiles 2', props.svelteFiles)
+      //recursion for elements like main,p,h1
+      if (props.children[i].type === "svelteElement") {
+        childList.push(
+          <FileNode
+            children={props.children[i].children}
+            fileName={props.children[i].tagName}
+            fileType={"svelteElement"}
+            svelteFiles={props.svelteFiles}
+            aliases = {aliases}
+          />
+        );
+      } else {
+        //HOW TO PERSIST ALIASES TO THINGS WE WANT IT TO
+        // AND NOT THINGS WE DON'T WANT IT TO
         // handles svelte components
 
         // SEND ERROR IF IT CANT FIND KEY/VALUE PAIR
 
+        // 
         // what we need to do:
         // SEND ARRAY OF CHILDREN FROM THE MATCHING SVELTEFILES OBJ AS PROPS TO REACT COMPONENT
         // iterate through the svelteFiles array
+//////////////////Stopped here thought about making aliases a state///////
 
-        /////////////clicking main causes it not to work///////////
-        /////////////////pick up here///////////////////
-        for (let i = 0 ; i < props.svelteFiles.length; i ++) {
-          // B.svelte === B.sveltea
-          // console.log(props.svelteFiles)
-          if (props.svelteFiles[i].fileName === aliases[props.children[i].tagName]){
-            childList.push(<FileNode children={props.svelteFiles[i].children} fileName={props.children[i].tagName} fileType={'svelteComponent'} svelteFiles={props.svelteFiles}/>)
+        console.log('INSIDE A SVELTE COMPONENT:', props.children[i].tagName);
+        console.log('test aliases', aliases)
+        // console.log('searching for alias:', aliases[props.children[i].tagName], 'type:', aliases[props.children[i].tagName])
+        // console.log("aliases:", aliases);
+
+
+        
+        let searchStr;
+        let hasAlias = false;
+        if(aliases[props.children[i].tagName]){
+          hasAlias = true;
+          searchStr = aliases[props.children[i].tagName];
+        }
+        
+        if(hasAlias){
+          for (let i = 0; i < props.svelteFiles.length; i++) {
+            console.log('searching svelte files....');
+            // console.log("tagname:", props.children[i].tagName);
+            //const svelteFileName = props.svelteFiles[i].fileName.toString();
+            // console.log('type of svelteFile:', typeof aliases[props.children[i].tagName])
+            // console.log('alias for this file:', aliases[props.children[i].tagName])
+            // console.log('the svelteFile ', props.svelteFiles[i])
+
+            console.log('looking at', props.svelteFiles[i].fileName, 'type:', typeof props.svelteFiles[i].fileName );
+            const string = props.svelteFiles[i].fileName;
+            
+
+
+            if ( string == searchStr) {
+              console.log('matchinggg!');
+
+              console.log(`Children for ${props.children[i].tagName}`, props.svelteFiles[i].children)
+              childList.push(
+                <FileNode 
+                children={props.svelteFiles[i].children} 
+                fileName={props.children[i].tagName} 
+                fileType={"svelteComponent"} 
+                svelteFiles={props.svelteFiles} 
+                aliases={aliases}/>
+              );
+
+             
+            }
           }
         }
-          // for each sveltefile, check if sveltefile.fileName is the same as aliases property value
-          // if it is true
-          // recursive call to <FileNode children = the children from fileNameObj  fileName=     svelteFiles = > sveltefile.children passing in the svelteFiles array as props again
-        
+        else {
+          childList.push(
+            <FileNode 
+            children={[]} 
+            fileName={props.children[i].tagName} 
+            fileType={"svelteComponent"} 
+            svelteFiles={props.svelteFiles} 
+            aliases={aliases}/>
+          );
+        }
           
-      //
-        // props.children[i].tagName = B
-        // tagName = B
-        // aliases = {B: B.svelte}
-        // props.svelteFiles = [{filename: B, children: [svelteElement,svelteComponent, svelteScript]}]
+        console.log('after test')
+        
 
-        
-        
-        
+
+
       }
-
     }
   }
 
-    // for(let i = 0; i < props.children.length; i++){
-
-    //   // ignore reading scripts for now
-    //   if(props.children[i].tagName === 'script') continue;
-    //   // alias handling, pass alias down as props for each element for reference
-
-    //   // console.log('yessir', (props.children[0].children[0].value).indexOf(props.children[i].tagName))
-    //   //ignore the combo of svelteComponent and svelteElement
-      
-    //   // different function if we run into svelte element
-
-    //   // console.log('this is what file looks like', file)
-
-    //   // if we go into an element we need to push to the last component we were in
-
-
-    // }
-  
-  //currently only see the components because we are looking through app.svelte
-    //every component does not have access to the sveltefiles array
-    // need to pass down sveltefiles array to the children to be 
-    // loop through newAliases
-      //check to see if the value in newaliass prop matches a svelte file name
-        // if it does
-         // loop through svelte files to find if there is a child
-            //if there is no child, the component is finished, move on to next component
-
-// why do we need aliases?
-// to know what file is really what it is, it knows it's name but doesn't know what it is.
-// if value is found in sveltefile
-// what component has what children
-// connect the renamed components 
-// compare alias to filename, compare children
-  // match between filename and the alias
-
-
-  // function getComponents(list){
-  //   console.log('list',list);
-  //   console.log(list[0].props.children);
-  //   for(let i = 0; i < list.length; i++){
-  //     if(list[i].props.children){
-  //       for(let j = 0; j < list[j].props.children.length; j++){
-  //         if(list[i].props.children[j].type === 'svelteComponent'){
-  //           finalList.push(list[i].props.children[j]);
-  //         }
-  //       }
-  //     }
-  //   }
 
   // recursion to continuously find children
   // if there are children, continuously invoke createTree on line 21
-  if(props.children){
+  if (props.children) {
     createTree();
   }
 
-// if there are no children left to parse, render/return. Don't need to create/continue tree if no children
+  // if there are no children left to parse, render/return. Don't need to create/continue tree if no children
 
-  return(
-  <div id='treeNode' >
-    { //if filetype is svelteelement, run code below
-    props.fileType === 'svelteElement'?
-    (
-      <div>
-        {
-          // if there is a length, if there is children
-          props.children.length?
-          (
-            // if current state of showchildren is truthy
-            showChildren?
-            // create up arrow icon
-            <button id='expandButton' onClick={() => setShow(!showChildren)}> <AiFillFolderOpen/>{props.fileName}<IoIosArrowUp className="arrow"/></button>
-            // otherwise create down arrow icon
-            : <button id='expandButton' onClick={() => setShow(!showChildren)}> <AiFillFolder/>{props.fileName}<IoIosArrowDown className="arrow"/></button>
-          )
-          // if no child don't show any button
-          : null
-        }
-
-        <div id='childrenNodes'>
-          {/* if showchildren is truthy, show childlist, otherwise show nothing */}
-          {showChildren? childList : null}
-        </div>
-      </div>
-    )
-
-        : //otherwise do this stuff
-      (
-      <div>
-        <div id='componentNode'>
-            Component
-            <div>
-              Name: {props.fileName}
-            </div>
-            <div>
-              Children: {props.children ? props.children.length : 0}
-            </div>
+  return (
+    <div id="treeNode">
+      {
+        //if filetype is svelteelement, run code below
+        props.fileType === "svelteElement" ? (
+          <div>
             {
-              props.children.length?
-              (
-                showChildren ?
-                <button id='expandButton' onClick={() => setShow(!showChildren)}><IoIosArrowUp className="arrow"/></button>
-                : <button id='expandButton' onClick={() => setShow(!showChildren)}><IoIosArrowDown className="arrow"/></button>
-              )
-              : null
+              // if there is a length, if there is children
+              props.children.length ? (
+                // if current state of showchildren is truthy
+                showChildren ? (
+                  // create up arrow icon
+                  <button
+                    id="expandButton"
+                    onClick={() => setShow(!showChildren)}
+                  >
+                    {" "}
+                    <AiFillFolderOpen />
+                    {props.fileName}
+                    <IoIosArrowUp className="arrow" />
+                  </button>
+                ) : (
+                  // otherwise create down arrow icon
+                  <button
+                    id="expandButton"
+                    onClick={() => setShow(!showChildren)}
+                  >
+                    {" "}
+                    <AiFillFolder />
+                    {props.fileName}
+                    <IoIosArrowDown className="arrow" />
+                  </button>
+                )
+              ) : // if no child don't show any button
+              null
             }
-        </div>
 
-        <div id='childrenNodes'>
-          {showChildren ? childList : null}
+            <div id="childrenNodes">
+              {/* if showchildren is truthy, show childlist, otherwise show nothing */}
+              {showChildren ? childList : null}
+            </div>
+          </div>
+        ) : (
+          //otherwise do this stuff
+          <div>
+            <div id="componentNode">
+              Component
+              <div>Name: {props.fileName}</div>
+              <div>Children: {props.children ? props.children.length : 0}</div>
+              {props.children.length ? (
+                showChildren ? (
+                  <button
+                    id="expandButton"
+                    onClick={() => setShow(!showChildren)}
+                  >
+                    <IoIosArrowUp className="arrow" />
+                  </button>
+                ) : (
+                  <button
+                    id="expandButton"
+                    onClick={() => setShow(!showChildren)}
+                  >
+                    <IoIosArrowDown className="arrow" />
+                  </button>
+                )
+              ) : null}
+            </div>
 
-        </div>
-      </div>
+            <div id="childrenNodes">{showChildren ? childList : null}</div>
+          </div>
         )
-    }
-
-  </div>
+      }
+    </div>
   );
-}
- ;
-
-
-
+};
 export default FileNode;
